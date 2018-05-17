ff9d990355110329d5b31f3ec4b63a4365602d6e
Deviluna <wuyuehuause@163.com>
2018-05-17 10:17:54 +0800

    simple demo

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+import os
+# -- coding: utf-8 --
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    pic_folder="static/pictures/"
+    pic_list=os.listdir(pic_folder)
+    pic_str=""
+    
+    for item in pic_list:
+        pic_str+="<img src='"+pic_folder+item+"'/><br>"+os.path.basename(item)+"<br>"
+        
+    return render_template('index.html')+pic_str
+
+
+
+if __name__=="__main__":
+    app.run(debug=True,host='0.0.0.0',port=5000)
